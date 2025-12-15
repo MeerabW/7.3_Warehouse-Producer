1805839efafbce910abdbe4fd5c4843e8a3c0d93
Meerab Wali <mwali@student.tgm.ac.at>
2025-12-15 20:59:32 +0100

    add warehouse producer draft

---
+++ src/main/java/at/ac/tgm/mwali/warehouseproducer/WarehouseProducerApplication.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.mwali.warehouseproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseProducerApplication.class, args);
+    }
+
+}
